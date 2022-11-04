78a50a06e5a36661a958bc0d80d9fcd9f8761d04
Vishnu Mano <vishnu.mano@gmail.com>
2022-11-04 14:39:30 -0400

    opencv

--- opencv/practice.py
+++ opencv/practice.py
@@ -1,8 +1,12 @@
 import cv2 as cv
 
-def rescale()
-
-img = cv.imread('Photos/img1.jpeg')
+img = cv.imread('Photos/img2.jpeg')
 cv.imshow('Dog', img)
+
+capture = cv.VideoCapture('Videos/vid1.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
 
 cv.waitKey(0)
